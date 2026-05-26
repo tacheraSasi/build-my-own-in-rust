a5a3edc18d1f75d8453d15f58a8919f0fe84b7f8
tacheraSasi <tacherasasi@gmail.com>
2026-05-26 14:36:53 +0300

    Implement ideal_approach to read from stdin or files

--- cat/src/main.rs
+++ cat/src/main.rs
@@ -1,12 +1,12 @@
 use std::{env, fs};
+use std::io::{self, Read};
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: cat <filepaths...>");
-        return;
-    }
-    noob_approach(args);
+    // noob_approach(args);
+    ideal_approach(args);
+    // systems_thinking_approach();
 }
 
 fn noob_approach(args: Vec<String>) {
@@ -21,6 +21,18 @@ fn noob_approach(args: Vec<String>) {
     }
 }
 
-fn ideal_approach(){}
+fn ideal_approach(args: Vec<String>) {
+    if args.len() == 1 {
+        let mut buffer = String::new();
+        io::stdin().read_to_string(&mut buffer).unwrap();
+        println!("{}", buffer);
+    } else {
+        let filepaths = &args[1..];
+        for path in filepaths {
+            let content = fs::read_to_string(path).unwrap();
+            println!("{}", content);
+        }
+    }
+}
 
-fn systems_thinking_approach(){}
+fn systems_thinking_approach() {}
