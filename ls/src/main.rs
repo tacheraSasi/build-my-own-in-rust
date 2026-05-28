63652574de888c091af536908858885a6792fd21
tacheraSasi <tacherasasi@gmail.com>
2026-05-28 16:11:30 +0300

    Refactor main logic into noob_approach function

--- ls/src/main.rs
+++ ls/src/main.rs
@@ -1,6 +1,8 @@
 use std::fs;
-
-fn main() {
+fn main(){
+    noob_approach()
+}
+fn noob_approach() {
     let paths = fs::read_dir(".").unwrap();
     for path in paths{
         println!("{:?}", path.unwrap().file_name().to_string_lossy());
