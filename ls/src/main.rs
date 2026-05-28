78c5c4ebc1f494c6759b653a900c266d4fcd8db3
tacheraSasi <tacherasasi@gmail.com>
2026-05-28 16:09:27 +0300

    List files in current directory in main function

--- ls/src/main.rs
+++ ls/src/main.rs
@@ -2,5 +2,7 @@ use std::fs;
 
 fn main() {
     let paths = fs::read_dir(".").unwrap();
-    
+    for path in paths{
+        println!("{:?}", path.unwrap().file_name().to_string_lossy());
+    }
 }
